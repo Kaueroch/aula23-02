9693ce731b4df32d4b867959f39bb352872032da
Kauê Alcantara Rocha <118750523+Kaueroch@users.noreply.github.com>
2024-02-23 12:49:37 -0300

    Add files via upload

---
+++ Notas.java
@@ -0,0 +1,25 @@
+package notas;
+
+import java.util.Scanner;
+
+public class Notas {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome;
+        String materia;
+        double n1, n2, n3, m;
+        System.out.println("Escola Fortec - NOTAS");
+        System.out.println("==============");
+        System.out.println("Nome: ");
+        nome = sc.nextLine();
+        System.out.println("matéria ");
+        materia = sc.nextLine();
+        System.out.println("nota 1");
+        n1= sc.nextDouble();
+         
+    }
+   
+    
+   
+}
